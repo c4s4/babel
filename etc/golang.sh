44daa93b5a271e447966a7af950c56d8d00869d1
Michel Casabianca <casa@sweetohm.net>
2019-12-19 11:24:01 +0100

    Fixed GOTOOLS configuration

--- etc/golang.sh
+++ etc/golang.sh
@@ -3,7 +3,7 @@
 
 export GOROOT=${APP_DIR}/go/current
 export GOPATH=${HOME}/.go
-export GOTOOLS=${GOPATH}/tol
+export GOTOOLS=${GOPATH}/.gotools
 export PATH=${GOPATH}/bin:${GOTOOLS}/bin:${APP_DIR}/go/current/bin:${PATH}
 # fix go on raspberry pi
 export GOARM=5
