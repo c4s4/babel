74016e89f3dadf0ff636f2545a2f1b00b18e0ae6
Michel Casabianca <casa@sweetohm.net>
2020-10-05 11:55:51 +0200

    Fixed scripts and confs to install

--- etc/app-dir-app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# ~/.profile.d/app.sh
-
-export APP_DIR=~/app
-export PATH=${APP_DIR}/bin:${PATH}
--- etc/app-dir.sh
+++ etc/app-dir.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # /etc/profile.d/app-dir-opt.sh
+# Set application directory (/opt or ~/app for instance)
 
 export APP_DIR=/opt
 export PATH=${APP_DIR}/bin:${PATH}
