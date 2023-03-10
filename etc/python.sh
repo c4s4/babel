c2498bda134371c93aae3bc9d300573b9d8a676a
Michel Casabianca <mcasabianca@sfbx.io>
2023-03-10 10:50:39 +0100

    Added Python local installation binaries directory in PATH

--- etc/python.sh
+++ etc/python.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # /etc/profile.d/python.sh
 
-export PATH=${APP_DIR}/python/current/bin:${PATH}
+export PATH=${HOME}/.local/bin/:${APP_DIR}/python/current/bin:${PATH}
 export PYTHONSTARTUP=${HOME}/.python.startup
